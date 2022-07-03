160ca08edbfdd47e899b1f991c5c8249fd9769fa
Ryan Parman <ryan@ryanparman.com>
2022-07-03 00:53:56 -0600

    Testing.

--- hooks/gofumpt.sh
+++ hooks/gofumpt.sh
@@ -6,9 +6,7 @@ if [ "${DEBUG}" != unset ]; then
     set -x
 fi
 
-printf '%q\n' "${PWD##*/}"
-curr_dir=${PWD##*/}     # to assign to a variable
-curr_dir=${curr_dir:-/} # to correct for the case where PWD=/
+curr_dir="$(basename "$(realpath "$0")")"
 
 # shellcheck disable=SC1091
 source "${curr_dir}/__install_go.sh"
