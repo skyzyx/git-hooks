28ef63df8a62ee91ad9c36dee4c7cb3a8708cae4
Ryan Parman <ryan@ryanparman.com>
2022-07-03 01:13:15 -0600

    Testing.

--- hooks/golangci-lint.sh
+++ hooks/golangci-lint.sh
@@ -21,5 +21,8 @@ if ! command -v golangci-lint > /dev/null 2>&1; then
     fi
 fi
 
-golangci-lint run --fix ./*.go
-golangci-lint run --fix ./**/*.go
+# 1. Find all .go files.
+# 2. Find the directories of each of the .go files.
+# 3. Run golangci-lint on each of the directories.
+# 4. But only process a directory once.
+find "$PWD" -type f -name "*.go" -print0 | xargs -0 -I% dirname "%" | uniq | xargs -0 -I% bash -c 'cd "%" && golangci-lint run --fix *.go'
