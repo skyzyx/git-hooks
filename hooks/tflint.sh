bf30ff31e1b910e79110db0be0a9159a430c9b85
Ryan Parman <ryan@ryanparman.com>
2022-07-05 15:08:55 -0600

    Testing.

--- hooks/tflint.sh
+++ hooks/tflint.sh
@@ -6,8 +6,6 @@ if [ "${DEBUG}" != unset ]; then
     set -x
 fi
 
-curr_dir="$(dirname "$(realpath "$0")")"
-
 if ! command -v tflint > /dev/null 2>&1; then
     if ! command -v brew > /dev/null 2>&1; then
         echo 'Attempting to install tflint from Homebrew...'
@@ -25,11 +23,8 @@ find "$PWD" -type d -name ".terraform" | xargs rm -Rf
 # Initialize tflint.
 GITHUB_TOKEN="" tflint --init
 
-# 1. Find all .tf files.
-# 2. Find the directories of each of the .tf files.
-# 3. Run tflint on each of the directories.
-# 4. But only process a directory once.
-find "$PWD" -type f \( -name "*.tf" -or -name "*.tfvars" \) -print0 |
-    xargs -0 -I% dirname "%" |
-    uniq |
-    xargs -I% bash -c 'cd "%" && terraform init && tflint --config="'"${curr_dir}"'/.tflint.hcl" .'
+# Initialize Terraform.
+terraform init
+
+# Run tflint over the whole directory.
+tflint .
