dec8659afdff42ea78ee4304275568716030c0a7
Ryan Parman <ryan@ryanparman.com>
2022-07-05 18:45:12 -0600

    Testing.

---
+++ hooks/checkov.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+readonly DEBUG=${DEBUG:-unset}
+if [ "${DEBUG}" != unset ]; then
+    set -x
+fi
+
+if ! command -v checkov > /dev/null 2>&1; then
+    echo 'Attempting to install checkov from Pypi...'
+    pip3 install checkov
+fi
+
+checkov --directory . --framework terraform --quiet --compact
